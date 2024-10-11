23d3ce491dafc9eb53fb9e517b008d935714b68d
jpress1409 <jpress1409@gmail.com>
2024-10-11 17:22:04 -0400

    Started Laying out the code

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,4 +1,22 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Date;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+       Date date = new Date();
+        Scanner scan = new Scanner(System.in);
+        String fileName = "transactions.csv";
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+           
+        }catch(IOException e){
+            System.err.println(e);
+        }
+
+    }
 }
